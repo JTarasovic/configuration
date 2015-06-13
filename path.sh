aab085aac0bd9afd8f9e42eed4f9565ac68896d2
Jason Tarasovic <jason@tarasovic.com>
2015-06-13 18:41:34 -0500

    add nvm related exports

--- path.sh
+++ path.sh
@@ -7,6 +7,12 @@
 [[ -d $HOME/android-studio/bin ]] && PATH=$PATH:$HOME/android-studio/bin
 [[ -d $HOME/Android/Sdk ]] && PATH=$PATH:$HOME/Android/Sdk/tools:$HOME/Android/Sdk/platform-tools
 
+
+# add go binaries to path
 [[ -d $HOME/Development/gocode/bin ]] && PATH=$PATH:$HOME/Development/gocode/bin
+
+
+# allow nvm to set up path
+[[ -d $NVM_DIR ]] && source $(brew --prefix nvm)/nvm.sh
 
 export PATH
