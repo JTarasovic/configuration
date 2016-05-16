69c19a274e985e58f0e06096ac8de46f2151541b
Jason Tarasovic <jason@tarasovic.com>
2016-05-16 09:50:46 +0400

    re-arrange and cleanup aliases

--- aliases.sh
+++ aliases.sh
@@ -31,7 +31,7 @@ if [ "$IS_LINUX" -eq 1 ]; then
     alias search='pacman -Ss'
   fi
 
-  alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert.*$//'\'')"'
+  # alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert.*$//'\'')"'
   alias l='ls --color=auto'
   alias l.='ls -d .* --color=auto'
   alias la='ls -la --color=auto'
@@ -46,30 +46,36 @@ else
   alias ll='ls -l'
   alias dtls='apropos dtrace'
 
+  # TODO: seperate out BSD (pkg) vs OSX (brew)
   #brew aliases
   alias update='brew update && brew upgrade'
   alias install='brew install'
   alias search='brew search'
 fi
 
-alias gs='git status'
-alias push='git push'
+# terminal manipulation
 alias c='clear'
 alias ..='cd ..'
 alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# git quick actions
+alias gs='git status'
+alias push='git push'
+
+# short version of longer commands
 alias speed='speedtest-cli'
-alias avd='emulator-x86 -avd avd1 -qemu -m 2047 -enable-kvm'
-# misc.
+alias pb='perlbrew'
+
+# long chains of things that i don't want to commit to long term memmory.
+alias flatten='find ./ -type f -exec mv '{}' . \;'
 alias hdeploy='git push heroku master && sleep 5 && heroku logs && heroku ps'
 alias server='python -m SimpleHTTPServer &'
-alias pb='perlbrew'
-alias flatten='find ./ -type f -exec mv '{}' . \;'
-
 # courtesy @igrigorik; open current gh repo in browser
 alias gh="open \`git remote -v | grep git@github.com | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/http:\/\//'\`"
 
 
-# testing
+# helpers for managing ebooks and such
 alias move_books="cd ~/staging/NZBin\ Complete && \
 ex_rename -e 'epub|mobi|pdf|pdb|azw|azw3|lit|rtf|doc|docx|chm' ~/staging/NZBin\ Complete ~/staging && \
 rmdir *(/)"
@@ -79,3 +85,4 @@ ex_rename -e nzb ~/Downloads ~/staging/nzbs && \
 rm *.zip && \
 open ~/staging/nzbs/*"
 alias sync_books="rsync -vurW --delete ~/Calibre\ Library/ freenas:/mnt/tank/media/Books"
+
