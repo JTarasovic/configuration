66ab3412edabbfb455c57676f4b53eee4ae3c9d7
Jason Tarasovic <jasontarasovic@mobilgov.com>
2017-04-17 14:54:12 -0500

    DRY - check if dir exists in pathmunge

--- path.sh
+++ path.sh
@@ -3,26 +3,20 @@
 # which is called for both bash and zsh
 
 pathmunge () {
-    if ! echo "$PATH" | egrep -q "(^|:)$1($|:)" ; then
-        PATH=$1:$PATH
+    if ! echo "$PATH" | grep -Eq "(^|:)$1($|:)" ; then
+        [ -d "$1" ] && PATH=$1:$PATH
     fi
 }
 
-# add android studio and adb to path
-# [[ -d $HOME/android-studio/bin ]] && PATH=$HOME/android-studio/bin:$PATH
-# [[ -d $HOME/Android/Sdk ]] && PATH=$HOME/Android/Sdk/tools:$HOME/Android/Sdk/platform-tools:$PATH
-
-# add local/bin to path
-[ -d "$HOME/.local/bin" ] && pathmunge "$HOME/.local/bin"
-[ -d /usr/local/bin ] && pathmunge "/usr/local/bin"
+pathmunge "$HOME"/android-studio/bin
+pathmunge "$HOME"/Android/Sdk/tools
+pathmunge "$HOME"/.local/bin
+pathmunge "$HOME"/.rvm/bin
+pathmunge "$HOME"/mongo/current/bin
+pathmunge /usr/local/bin
+pathmunge /usr/local/heroku/bin
 
 # add go binaries to path
 command -v go > /dev/null 2>&1 && pathmunge "$(go env GOPATH)/bin"
-
-# Add RVM to PATH for scripting
-[ -d "$HOME/.rvm/bin/" ] && pathmunge "$HOME/.rvm/bin"
-
-# Heroku Toolbelt
-[ -d /usr/local/heroku/bin ] && pathmunge "/usr/local/heroku/bin"
 
 export PATH
